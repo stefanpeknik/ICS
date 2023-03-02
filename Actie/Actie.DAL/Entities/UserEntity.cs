0d45fea878d2008c93d8dc3a31f5f0266d452858
Jurgee <jiristipek@email.cz>
2023-03-02 19:42:12 +0100

    add User def stats

--- Actie/Actie.DAL/Entities/UserEntity.cs
+++ Actie/Actie.DAL/Entities/UserEntity.cs
@@ -7,6 +7,11 @@ public class UserEntity : IEntity
     public required string Name { get; set; }
     public required string Surname { get; set; }
     public Uri? Photo { get; set; }
+    public int Age { get; set; }
+    public string? Gender { get; set; }
+    public float? Weight { get; set; }
+    public int? Height { get; set; }
+    public float? BMI { get; set; }
     public ICollection<ActivityEntity>? Activities { get; set; }
     public ICollection<ProjectEntity>? Projects { get; set; }
 
