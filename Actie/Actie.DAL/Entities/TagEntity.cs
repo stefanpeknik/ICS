3ae07b7352ff66d9fefeb6678fb7d35563a93b3d
Jurgee <jiristipek@email.cz>
2023-03-02 20:11:47 +0100

    remove Color from TagEntity

--- Actie/Actie.DAL/Entities/TagEntity.cs
+++ Actie/Actie.DAL/Entities/TagEntity.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Actie.DAT.Entities;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing.
 
 namespace Actie.DAL.Entities
 {
-    internal class TagEntity
+    public class TagEntity : IEntity
     {
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public required int? Color { get; set; }
+
     }
 }
