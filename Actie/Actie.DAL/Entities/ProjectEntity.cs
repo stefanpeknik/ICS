684b266205848d24877932d5ac448a04df626b5b
Jurgee <jiristipek@email.cz>
2023-03-02 19:48:45 +0100

    add Project def stats

--- Actie/Actie.DAL/Entities/ProjectEntity.cs
+++ Actie/Actie.DAL/Entities/ProjectEntity.cs
@@ -7,6 +7,7 @@ public class ProjectEntity : IEntity
     public required Guid Id { get; set; }
         
     public required string Name { get; set; }
+    public required string Description { get; set; }
     public ICollection<ActivityEntity>? Users { get; set; }
     public ICollection<ActivityEntity>? Activities { get; set; }
 
