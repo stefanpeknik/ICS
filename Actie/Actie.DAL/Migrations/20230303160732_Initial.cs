9feb39e8722e41d68e09673e56204cff1929a819
Jurgee <jiristipek@email.cz>
2023-03-03 17:08:06 +0100

    add first migration

---
+++ Actie/Actie.DAL/Migrations/20230303160732_Initial.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Actie.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Surname = table.Column<string>(type: "TEXT", nullable: false),
+                    Photo = table.Column<string>(type: "TEXT", nullable: true),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Gender = table.Column<string>(type: "TEXT", nullable: true),
+                    Weight = table.Column<float>(type: "REAL", nullable: true),
+                    Height = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    UserEntityId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Projects_UserEntity_UserEntityId",
+                        column: x => x.UserEntityId,
+                        principalTable: "UserEntity",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Start = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    End = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Rating = table.Column<int>(type: "INTEGER", nullable: true),
+                    Type = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ProjectEntityId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    ProjectEntityId1 = table.Column<Guid>(type: "TEXT", nullable: true),
+                    UserEntityId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Activities_Projects_ProjectEntityId",
+                        column: x => x.ProjectEntityId,
+                        principalTable: "Projects",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Activities_Projects_ProjectEntityId1",
+                        column: x => x.ProjectEntityId1,
+                        principalTable: "Projects",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Activities_UserEntity_UserEntityId",
+                        column: x => x.UserEntityId,
+                        principalTable: "UserEntity",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TagEntity",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    ActivityEntityId = table.Column<Guid>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TagEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TagEntity_Activities_ActivityEntityId",
+                        column: x => x.ActivityEntityId,
+                        principalTable: "Activities",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_ProjectEntityId",
+                table: "Activities",
+                column: "ProjectEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_ProjectEntityId1",
+                table: "Activities",
+                column: "ProjectEntityId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Activities_UserEntityId",
+                table: "Activities",
+                column: "UserEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_UserEntityId",
+                table: "Projects",
+                column: "UserEntityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TagEntity_ActivityEntityId",
+                table: "TagEntity",
+                column: "ActivityEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TagEntity");
+
+            migrationBuilder.DropTable(
+                name: "Activities");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "UserEntity");
+        }
+    }
+}
