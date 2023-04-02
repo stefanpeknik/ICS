150bf779e7aec5d3cee50b7c0bfe2077d4b769b1
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-02 10:00:09 +0200

    add IEntityDTO class

---
+++ Actie/Actie.DAL/DTOs/IEntityDTO.cs
@@ -0,0 +1,9 @@
+﻿
+using Actie.DAL.Entities;
+
+namespace Actie.DAL.DTOs;
+public interface IEntityDTO<in TEntity>
+    where TEntity : IEntity
+{
+    void MapToExistingEntity(TEntity existingEntity, TEntity newEntity);
+}
