8efcd012b20298253e45093eee474b4f3a6bf6d0
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-02 15:01:25 +0200

    add IRepository class

---
+++ Actie/Actie.DAL/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actie.DAL.Entities;
+
+namespace Actie.DAL.Repositories;
+public interface IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    IQueryable<TEntity> Get();
+    void Delete(Guid entityId);
+    ValueTask<bool> ExistsAsync(TEntity entity);
+    Task<TEntity> InsertAsync(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
+}
