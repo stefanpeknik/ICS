ac45e18b251e9d70bbfac3b96c575eaf09cb6fb9
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-02 10:34:55 +0200

    add UserEntityDTO class

---
+++ Actie/Actie.DAL/DTOs/UserEntityDTO.cs
@@ -0,0 +1,18 @@
+﻿
+using Actie.DAL.Entities;
+
+namespace Actie.DAL.DTOs;
+public class UserEntityDTO : IEntityDTO<UserEntity>
+{
+    public void MapToExistingEntity(UserEntity existingEntity, UserEntity newEntity)
+    {
+        existingEntity.Name = newEntity.Name;
+        existingEntity.Surname = newEntity.Surname;
+        existingEntity.Photo = newEntity.Photo;
+        existingEntity.Age = newEntity.Age;
+        existingEntity.Gender = newEntity.Gender;
+        existingEntity.Weight = newEntity.Weight;
+        existingEntity.Height = newEntity.Height;
+    }
+}
+
