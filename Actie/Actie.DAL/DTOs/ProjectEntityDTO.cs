b1d6c797e1a2483cbb1b8a8bca97d70b1de03535
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-02 10:35:11 +0200

    add ProjectEntityDTO class

---
+++ Actie/Actie.DAL/DTOs/ProjectEntityDTO.cs
@@ -0,0 +1,12 @@
+﻿
+using Actie.DAL.Entities;
+
+namespace Actie.DAL.DTOs;
+internal class ProjectEntityDTO : IEntityDTO<ProjectEntity>
+{
+    public void MapToExistingEntity(ProjectEntity existingEntity, ProjectEntity newEntity)
+    {
+        existingEntity.Name= newEntity.Name;
+        existingEntity.Description= newEntity.Description;
+    }
+}
