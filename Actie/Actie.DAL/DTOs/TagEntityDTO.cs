3cb12b71eea9d2e47446f66079562359507bf5a1
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-02 10:34:40 +0200

    add TagEntityDTO class

---
+++ Actie/Actie.DAL/DTOs/TagEntityDTO.cs
@@ -0,0 +1,12 @@
+﻿
+using Actie.DAL.Entities;
+
+namespace Actie.DAL.DTOs;
+public class TagEntityDTO : IEntityDTO<TagEntity>
+{
+    public void MapToExistingEntity(TagEntity existingEntity, TagEntity newEntity)
+    {
+        existingEntity.Name = newEntity.Name;
+        existingEntity.Description = newEntity.Description;
+    }
+}
