b7380802a06eac3f2f604af5a59a2c72eb7127eb
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-02 15:55:36 +0200

    create Repository.cs

---
+++ Actie/Actie.DAL/Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿
+using Actie.DAL.DTOs;
+using Actie.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actie.DAL.Repositories;
+public class Repository<TEntity> : IRepository<TEntity>
+    where TEntity : class, IEntity
+{
+    private readonly DbSet<TEntity> _dbSet;
+    private readonly IEntityDTO<TEntity> _entityDTO;
+
+    public Repository(DbContext dbContext, IEntityDTO<TEntity> entityDTO)
+    {
+        _dbSet = dbContext.Set<TEntity>();
+        _entityDTO = entityDTO;
+    }
+    public IQueryable<TEntity> Get() => _dbSet;
+
+    public async ValueTask<bool> ExistsAsync(TEntity entity)
+        => entity.Id != Guid.Empty && await _dbSet.AnyAsync(e => e.Id == entity.Id);
+
+    public async Task<TEntity> InsertAsync(TEntity entity)
+        => (await _dbSet.AddAsync(entity)).Entity;
+
+    public async Task<TEntity> UpdateAsync(TEntity entity)
+    {
+        TEntity existingEntity = await _dbSet.SingleAsync(e => e.Id == entity.Id);
+        _entityDTO.MapToExistingEntity(existingEntity, entity);
+        return existingEntity;
+    }
+
+    public void Delete(Guid entityId) => _dbSet.Remove(_dbSet.Single(i => i.Id == entityId));
+}
