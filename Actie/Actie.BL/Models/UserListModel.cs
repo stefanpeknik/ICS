c1a7365b926c770f8b19b5917cb5404a3626acf7
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-04 23:40:49 +0200

    add User Detail/List Model

---
+++ Actie/Actie.BL/Models/UserListModel.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actie.BL.Models;
+public record UserListModel : ModelBase
+{
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public Uri? Photo { get; set; }
+
+    public static UserListModel Empty => new()
+    {
+        Id = Guid.NewGuid(),
+        Name = string.Empty,
+        Surname = string.Empty,
+        Photo = null,
+    };
+}
