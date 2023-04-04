9d380f3a9b17e996760d9a35cd73b5e48fb0a4c0
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-04 23:58:17 +0200

    add Project Detail/List Model

---
+++ Actie/Actie.BL/Models/ProjectDetailModel.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actie.DAL.Entities;
+
+namespace Actie.BL.Models;
+public record ProjectDetailModel : ModelBase
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public ObservableCollection<ActivityEntity>? Activities { get; init; } = new ();
+    public ObservableCollection<UserProjectEntity>? Users { get; init; } = new();
+
+    public static ProjectDetailModel Empty => new()
+    {
+        Id = Guid.NewGuid(),
+        Name = string.Empty,
+        Description = string.Empty,
+
+    };
+}
---
+++ Actie/Actie.BL/Models/ProjectListModel.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actie.BL.Models;
+public record ProjectListModel : ModelBase
+{
+    public required string Name { get; set; }
+    public static ProjectListModel Empty => new()
+    {
+        Id = Guid.NewGuid(),
+        Name = string.Empty,
+    };
+}
