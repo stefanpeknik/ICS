14518d84f35fd11626e3cb32474c9e4bc8cfece2
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-04 23:39:34 +0200

    add Facade

---
+++ Actie/Actie.BL/Facades/IFacade.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actie.BL.Models;
+using Actie.DAL.Entities;
+
+namespace Actie.BL.Facades;
+public interface IFacade<TEntity, TListModel, TDetailModel>
+    where TEntity : class, IEntity
+    where TListModel : IModel
+    where TDetailModel : class, IModel
+{
+    Task DeleteAsync(Guid id);
+    Task<TDetailModel?> GetAsync(Guid id);
+    Task<IEnumerable<TListModel>> GetAsync();
+    Task<TDetailModel> SaveAsync(TDetailModel model);
+}
