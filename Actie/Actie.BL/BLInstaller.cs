cda4afa9880cf90dd584cc9b9fba6db8caea0441
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-04 23:41:06 +0200

    BusinessLogic

---
+++ Actie/Actie.BL/BLInstaller.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Actie.BL;
+public static class BLInstaller
+{
+    public static IServiceCollection AddBLServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IUnitOfWorkFactory, UnitOfWorkFactory>();
+
+        services.Scan(selector => selector
+            .FromAssemblyOf<BusinessLogic>()
+            .AddClasses(filter => filter.AssignableTo(typeof(IFacade<,,>)))
+            .AsMatchingInterface()
+            .WithSingletonLifetime());
+
+        services.Scan(selector => selector
+            .FromAssemblyOf<BusinessLogic>()
+            .AddClasses(filter => filter.AssignableTo(typeof(ModelMapperBase<,,>)))
+            .AsMatchingInterface()
+            .WithSingletonLifetime());
+
+        return services;
+    }
+}
+
