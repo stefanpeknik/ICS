b3dc47b6d8680bbd0c1d30be6687d5757e87646a
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-04 23:58:52 +0200

    modify User DetailModel

--- Actie/Actie.BL/Models/UserDetailModel.cs
+++ Actie/Actie.BL/Models/UserDetailModel.cs
@@ -4,12 +4,31 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Actie.DAL.Entities;
 
 namespace Actie.BL.Models;
 public record UserDetailModel : ModelBase
 {
+    public required string Name { get; set; }
+    public required string Surname { get; set; }
+    public Uri? Photo { get; set; }
+    public int? Age { get; set; }
+    public string? Gender { get; set; }
+    public float? Weight { get; set; }
+    public int? Height { get; set; }
+    public ObservableCollection<ActivityEntity>? Activities { get; init; } = new();
+    public ObservableCollection<UserProjectEntity>? Projects { get; init; } = new();
 
+    public static UserDetailModel Empty => new()
+    {
+        Id = Guid.NewGuid(),
+        Name = string.Empty,
+        Surname = string.Empty,
+        Photo = null,
+        Gender = string.Empty,
+    };
 }
