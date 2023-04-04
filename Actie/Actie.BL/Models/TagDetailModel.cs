7cbcdaf48f46bd5ffc2ac05a77f13182642f3878
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-04 23:58:26 +0200

    add Tag Detail/List Model

---
+++ Actie/Actie.BL/Models/TagDetailModel.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actie.DAL.Entities;
+
+namespace Actie.BL.Models;
+public record TagDetailModel : ModelBase
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    public ObservableCollection<ActivityTagEntity> Activities { get; init; } = new();
+
+    public static TagDetailModel Empty => new()
+    {
+        Id = Guid.NewGuid(),
+        Name = string.Empty,
+        Description = string.Empty,
+    };
+}
---
+++ Actie/Actie.BL/Models/TagListModel.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actie.BL.Models;
+public record TagListModel : ModelBase
+{
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+
+    public static TagListModel Empty => new()
+    {
+        Id = Guid.NewGuid(),
+        Name = string.Empty,
+        Description = string.Empty,
+    };
+}
