cbf5ec19ea0633d2f11f4af7137084202cb90f67
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-05 20:43:45 +0200

    add IActivityFacade

---
+++ Actie/Actie.BL/Facades/Interfaces/IActivityFacade.cs
@@ -0,0 +1,8 @@
+﻿
+using Actie.BL.Models;
+using Actie.DAL.Entities;
+
+namespace Actie.BL.Facades.Interfaces;
+public interface IActivityFacade : IFacade<ActivityEntity, ActivityListModel, ActivityDetailModel>
+{
+}
