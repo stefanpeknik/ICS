e844c8bd1671a4fcbadee6bedf56aeaf973c95a8
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-05 21:03:07 +0200

    add ITagModelMapper

---
+++ Actie/Actie.BL/Mappers/Interfaces/ITagModelMapper.cs
@@ -0,0 +1,10 @@
+﻿
+
+using Actie.BL.Models;
+using Actie.DAL.Entities;
+using CookBook.BL.Mappers;
+
+namespace Actie.BL.Mappers.Interfaces;
+public interface ITagModelMapper : IModelMapper<TagEntity, TagListModel, TagDetailModel>
+{
+}
