d42e45ade0019a9d8fda8858f97e80800ee5e52d
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-05 21:03:20 +0200

    add IActivityModelMapper

---
+++ Actie/Actie.BL/Mappers/Interfaces/IActivityModelMapper.cs
@@ -0,0 +1,9 @@
+﻿
+using Actie.BL.Models;
+using Actie.DAL.Entities;
+using CookBook.BL.Mappers;
+
+namespace Actie.BL.Mappers.Interfaces;
+public interface IActivityModelMapper : IModelMapper<ActivityEntity, ActivityListModel, ActivityDetailModel>
+{
+}
