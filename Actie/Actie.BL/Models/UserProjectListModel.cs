2d4f2ef0c00b5e6dc8176202b230969031bc717a
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-05 13:05:10 +0200

    add UserProject Detail/List Model

---
+++ Actie/Actie.BL/Models/UserProjectListModel.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actie.BL.Models;
+public record UserProjectListModel : ModelBase
+{
+    public required Guid UserId { get; set; }
+
+    public required Guid ProjectId { get; set; }
+
+    public static UserProjectListModel Empty => new()
+    {
+        UserId = Guid.Empty,
+        ProjectId = Guid.Empty,
+    };
+}
