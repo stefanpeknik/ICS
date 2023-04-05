22db73ccaaa6a12a2d440448c76a650251bff66b
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-05 20:43:55 +0200

    add IUserFacade

---
+++ Actie/Actie.BL/Facades/Interfaces/IUserFacade.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Actie.BL.Models;
+using Actie.DAL.Entities;
+
+namespace Actie.BL.Facades.Interfaces;
+public interface IUserFacade : IFacade<UserEntity, UserListModel, UserDetailModel>
+{
+}
