779a5a01c2186176e3c2760cca083774b8d0a8ab
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-05 13:05:21 +0200

    add ActivityTag Detail/List Model

---
+++ Actie/Actie.BL/Models/ActivityTagDetailModel.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actie.BL.Models;
+public record ActivityTagDetailModel : ModelBase
+{
+    public required Guid TagId { get; set; }
+    public required Guid ActivityId { get; set; }
+
+    public static ActivityTagDetailModel Empty => new()
+    {
+        ActivityId = Guid.Empty,
+        TagId = Guid.Empty
+    };
+}
