08d6f8174e18e17db9ed18ecc03ea96b7090844f
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-04-05 04:53:52 +0200

    remove redundant code

--- Actie/Actie.DAL/DTOs/IEntityDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using Actie.DAL.Entities;
-
-namespace Actie.DAL.DTOs;
-public interface IEntityDTO<in TEntity>
-    where TEntity : IEntity
-{
-    void MapToExistingEntity(TEntity existingEntity, TEntity newEntity);
-}
