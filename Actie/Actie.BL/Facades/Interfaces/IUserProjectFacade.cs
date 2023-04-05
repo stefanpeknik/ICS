1a473f60fcf9b137aadb1856092492513fe1d793
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-04-05 21:23:05 +0200

    add IUserProjectFacade

---
+++ Actie/Actie.BL/Facades/Interfaces/IUserProjectFacade.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Actie.BL.Models;
+
+namespace Actie.BL.Facades.Interfaces;
+public interface IUserProjectFacade
+{
+    Task<UserProjectDetailModel> SaveAsync(UserProjectDetailModel model, Guid )
+}
