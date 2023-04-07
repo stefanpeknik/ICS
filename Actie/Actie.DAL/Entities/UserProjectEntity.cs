33434ff2dc476ea41399e50aadaf42dfcacfcb78
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-04-07 20:39:47 +0200

    fix stuff in UserProjectEntity

--- Actie/Actie.DAL/Entities/UserProjectEntity.cs
+++ Actie/Actie.DAL/Entities/UserProjectEntity.cs
@@ -5,8 +5,8 @@ public record UserProjectEntity : IEntity
     public required Guid Id { get; set; }
 
     public required Guid UserId { get; set; }
-    public required UserEntity? User { get; set; }
+    public UserEntity? User { get; set; }
 
     public required Guid ProjectId { get; set; }
-    public required ProjectEntity? Project { get; set; }
+    public ProjectEntity? Project { get; set; }
 }
