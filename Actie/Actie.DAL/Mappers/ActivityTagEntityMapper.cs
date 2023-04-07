46d57dac24829171fa7ebd9a9305bbfa2d213841
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-04-07 22:14:49 +0200

    fix activitytagentitymapper

--- Actie/Actie.DAL/Mappers/ActivityTagEntityMapper.cs
+++ Actie/Actie.DAL/Mappers/ActivityTagEntityMapper.cs
@@ -7,8 +7,6 @@ public class ActivityTagEntityMapper : IEntityMapper<ActivityTagEntity>
     public void MapToExistingEntity(ActivityTagEntity existingEntity, ActivityTagEntity newEntity)
     {
         existingEntity.ActivityId = newEntity.ActivityId;
-        existingEntity.Activity = newEntity.Activity;
         existingEntity.TagId = newEntity.TagId;
-        existingEntity.Tag = newEntity.Tag;
     }
 }
