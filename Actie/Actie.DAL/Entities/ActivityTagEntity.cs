dcd746d4bc6eb686f00528e3828a0cb94fb75f85
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-04-07 19:17:43 +0200

    fix ActivityTagEntity

--- Actie/Actie.DAL/Entities/ActivityTagEntity.cs
+++ Actie/Actie.DAL/Entities/ActivityTagEntity.cs
@@ -5,8 +5,8 @@ public record ActivityTagEntity : IEntity
     public required Guid Id { get; set; }
 
     public required Guid ActivityId { get; set; }
-    public required ActivityEntity Activity { get; set; }
+    public ActivityEntity? Activity { get; init; }
 
     public required Guid TagId { get; set; }
-    public required TagEntity Tag { get; set; }
+    public TagEntity? Tag { get; init; }
 }
