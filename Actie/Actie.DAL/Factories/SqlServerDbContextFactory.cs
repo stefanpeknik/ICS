2caba6877465cf17b9367eb2d75c6d0674ca1e33
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-05-04 21:12:19 +0200

    create SqlServerDbCF

---
+++ Actie/Actie.DAL/Factories/SqlServerDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Actie.DAL.Factories;
+public class SqlServerDbContextFactory : IDbContextFactory<ActieDbContext>
+{
+    private readonly bool _seedDemoData;
+    private readonly DbContextOptionsBuilder<ActieDbContext> _contextOptionsBuilder = new();
+
+    public SqlServerDbContextFactory(string connectionString, bool seedDemoData = false)
+    {
+        _seedDemoData = seedDemoData;
+
+        _contextOptionsBuilder.UseSqlServer(connectionString);
+
+        ////Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
+        //_contextOptionsBuilder.LogTo(System.Console.WriteLine);
+        //_contextOptionsBuilder.EnableSensitiveDataLogging();
+    }
+
+    public ActieDbContext CreateDbContext() => new(_contextOptionsBuilder.Options, _seedDemoData);
+}
