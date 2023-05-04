73c6ac5643ae426805d8040675d0c96379a93a51
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-05-04 21:12:41 +0200

    fix DbConSQLiteFac

--- Actie/Actie.DAL/Factories/DbContextSQLiteFactory.cs
+++ Actie/Actie.DAL/Factories/DbContextSQLiteFactory.cs
@@ -2,32 +2,23 @@
 
 namespace Actie.DAL.Factories;
 
-public class DbContextSQLiteFactory
+public class DbContextSqLiteFactory : IDbContextFactory<ActieDbContext>
 {
-    public class DbContextSqLiteFactory : IDbContextFactory<ActieDbContext>
+    private readonly bool _seedTestingData;
+    private readonly DbContextOptionsBuilder<ActieDbContext> _contextOptionsBuilder = new();
+
+    public DbContextSqLiteFactory(string databaseName, bool seedTestingData = false)
     {
-        private readonly string _databaseName;
-        private readonly bool _seedTestingData;
+        _seedTestingData = seedTestingData;
 
-        public DbContextSqLiteFactory(string databaseName, bool seedTestingData = false)
-        {
-            _databaseName = databaseName;
-            _seedTestingData = seedTestingData;
-        }
+        ////May be helpful for ad-hoc testing, not drop in replacement, needs some more configuration.
+        //builder.UseSqlite($"Data Source =:memory:;");
+        _contextOptionsBuilder.UseSqlite($"Data Source={databaseName};Cache=Shared");
 
-        public ActieDbContext CreateDbContext()
-        {
-            DbContextOptionsBuilder<ActieDbContext> builder = new();
-
-            ////May be helpful for ad-hoc testing, not drop in replacement, needs some more configuration.
-            //builder.UseSqlite($"Data Source =:memory:;");
-            builder.UseSqlite($"Data Source={_databaseName};Cache=Shared");
-
-            ////Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
-            //builder.EnableSensitiveDataLogging();
-            //builder.LogTo(Console.WriteLine); 
-
-            return new ActieDbContext(builder.Options, _seedTestingData);
-        }
+        ////Enable in case you want to see tests details, enabled may cause some inconsistencies in tests
+        //_contextOptionsBuilder.EnableSensitiveDataLogging();
+        //_contextOptionsBuilder.LogTo(Console.WriteLine);
     }
+
+    public ActieDbContext CreateDbContext() => new(_contextOptionsBuilder.Options, _seedTestingData);
 }
