76338cc6a3d445d80d7856c7e06ca5583029274c
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-05-04 21:42:41 +0200

    add ViewModelBase

---
+++ Actie/Actie.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Java.Lang.Annotation;
+using Actie.App.Services;
+
+namespace Actie.App.ViewModels;
+public abstract class ViewModelBase : ObservableRecipient, IViewModel
+{
+    private bool _isRefreshRequired = true;
+
+    protected readonly IMessengerService MessengerService;
+
+    protected ViewModelBase(IMessengerService messengerService)
+        : base(messengerService.Messenger)
+    {
+        MessengerService = messengerService;
+        IsActive = true;
+    }
+
+    public async Task OnAppearingAsync()
+    {
+        if (_isRefreshRequired)
+        {
+            await LoadDataAsync();
+
+            _isRefreshRequired = false;
+        }
+    }
+
+    protected virtual Task LoadDataAsync()
+        => Task.CompletedTask;
+}
