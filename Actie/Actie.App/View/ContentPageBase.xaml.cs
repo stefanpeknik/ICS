68a77adb970d6d66fad322337d0b13871e87a48d
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-05-04 21:34:32 +0200

    add ContentPageBase

---
+++ Actie/Actie.App/View/ContentPageBase.xaml.cs
@@ -0,0 +1,23 @@
+using AndroidX.Lifecycle;
+using Actie.App.ViewModels;
+
+namespace Actie.App.View;
+
+public partial class ContentPageBase
+{
+    protected IViewModel ViewModel { get; }
+
+    public ContentPageBase(IViewModel viewModel)
+    {
+        InitializeComponent();
+
+        BindingContext = ViewModel = viewModel;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        await ViewModel.OnAppearingAsync();
+    }
+}
