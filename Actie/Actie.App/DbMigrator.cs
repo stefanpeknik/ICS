8e394efc346ddad0b60e6724d5747c1b25b12dfb
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-05-04 20:55:35 +0200

    add DbMigrator

---
+++ Actie/Actie.App/DbMigrator.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Actie.DAL;
+using Actie.App.Options;
+
+namespace Actie.App;
+interface IDbMigrator
+{
+    public void Migrate();
+    public Task MigrateAsync(CancellationToken cancellationToken);
+
+}
+public class NoneDbMigrator : IDbMigrator
+{
+    public void Migrate() { }
+    public Task MigrateAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
+
+public class SqliteDbMigrator : IDbMigrator
+{
+    private readonly IDbContextFactory<ActieDbContext> _dbContextFactory;
+    private readonly SqliteOptions _sqliteOptions;
+
+    public SqliteDbMigrator(IDbContextFactory<ActieDbContext> dbContextFactory, DALOptions dalOptions)
+    {
+        _dbContextFactory = dbContextFactory;
+        _sqliteOptions = dalOptions.Sqlite ?? throw new ArgumentNullException(nameof(dalOptions), $@"{nameof(DALOptions.Sqlite)} are not set");
+    }
+
+    public void Migrate() => MigrateAsync(CancellationToken.None).GetAwaiter().GetResult();
+
+    public async Task MigrateAsync(CancellationToken cancellationToken)
+    {
+        await using ActieDbContext dbContext = await _dbContextFactory.CreateDbContextAsync(cancellationToken);
+
+        if (_sqliteOptions.RecreateDatabaseEachTime)
+        {
+            await dbContext.Database.EnsureDeletedAsync(cancellationToken);
+        }
+
+        // Ensures that database is created applying the latest state
+        // Application of migration later on may fail
+        // If you want to use migrations, you should create database by calling  dbContext.Database.MigrateAsync(cancellationToken) instead
+        await dbContext.Database.EnsureCreatedAsync(cancellationToken);
+    }
+}
+
