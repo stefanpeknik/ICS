cd08882d6e2918c0964ec96d5fc152e86368af4f
Jiří Prokop <xproko47@vutbr.cz>
2023-05-06 18:52:12 +0200

    ADD DAL services loading in MauiProgram

--- Actie/Actie.App/MauiProgram.cs
+++ Actie/Actie.App/MauiProgram.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
+using System.Reflection;
+using Actie.App.Services;
 using CommunityToolkit.Maui;
 using Actie.BL;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Actie.App;
@@ -23,17 +26,50 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-        // Continue initializing your .NET MAUI App here
+        ConfigureAppSettings(builder);
+        
 
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
 
         builder.Services
-            //.AddDALServices(builder.Configuration) // TODO
+            .AddDALServices(builder.Configuration)
             .AddAppServices()
             .AddBLServices();
 
-        return builder.Build();
+        var app = builder.Build();
+
+        app.Services.GetRequiredService<IDbMigrator>().Migrate();
+        RegisterRouting(app.Services.GetRequiredService<INavigationService>());
+
+        return app;
+    }
+
+
+    private static void ConfigureAppSettings(MauiAppBuilder builder)
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+
+        var assembly = Assembly.GetExecutingAssembly();
+        const string appSettingsFilePath = "Actie.App.appsettings.json";
+        using var appSettingsStream = assembly.GetManifestResourceStream(appSettingsFilePath);
+        if (appSettingsStream is not null)
+        {
+            configurationBuilder.AddJsonStream(appSettingsStream);
+        }
+
+        var configuration = configurationBuilder.Build();
+        builder.Configuration.AddConfiguration(configuration);
+    }
+
+    private static void RegisterRouting(INavigationService navigationService)
+    {
+        foreach (var route in navigationService.Routes)
+        {
+            Routing.RegisterRoute(route.Route, route.ViewType);
+        }
     }
 }
+
+
