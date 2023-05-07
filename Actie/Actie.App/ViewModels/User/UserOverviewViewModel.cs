7151c60d5e1061af269fa5994bda40d37c00257a
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-05-07 20:30:54 +0200

    redo home page

--- Actie/Actie.App/ViewModels/User/UserOverviewViewModel.cs
+++ Actie/Actie.App/ViewModels/User/UserOverviewViewModel.cs
@@ -8,42 +8,28 @@ using Actie.BL.Models;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace Actie.App.ViewModels;
+
+[QueryProperty(nameof(Id), nameof(Id))]
 public partial class UserOverviewViewModel : ViewModelBase, IRecipient<UserEditMessage>, IRecipient<UserDeleteMessage>
 {
     private readonly IUserFacade _userFacade;
-    private readonly INavigationService _navigationService;
+
+    public Guid Id { get; set; }
 
     [ObservableProperty]
-    private IEnumerable<UserListModel> users = Array.Empty<UserListModel>();
+    public UserDetailModel user;
 
-    public UserOverviewViewModel(
-        IUserFacade userFacade,
-        INavigationService navigationService,
-        IMessengerService messengerService)
+    public UserOverviewViewModel(IUserFacade userFacade, IMessengerService messengerService)
         : base(messengerService)
     {
         _userFacade = userFacade;
-        _navigationService = navigationService;
     }
 
     protected override async Task LoadDataAsync()
     {
         await base.LoadDataAsync();
 
-        Users = await _userFacade.GetAsync();
-    }
-
-
-    [RelayCommand]
-    private async Task GoToAddUserAsync()
-    {
-        await _navigationService.GoToAsync("//users/add");
-    }
-
-    [RelayCommand]
-    private async Task LogInAsync()
-    {
-        await _navigationService.GoToAsync("//project/overview");
+        User = (await _userFacade.GetAsync(Id))!;
     }
 
     public async void Receive(UserEditMessage message)
