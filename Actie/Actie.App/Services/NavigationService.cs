bf3a25d35c2e29aa7ccfc59c64c7ac71e8bfcf34
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-05-07 21:16:06 +0200

    add Navigations

--- Actie/Actie.App/Services/NavigationService.cs
+++ Actie/Actie.App/Services/NavigationService.cs
@@ -6,6 +6,7 @@ using Actie.App.ViewModels;
 using Actie.App.Views;
 using Actie.App.Views.Activity;
 using Actie.App.Views.Project;
+using Actie.App.Views.Tag;
 using Actie.App.Views.User;
 namespace Actie.App.Services;
 public class NavigationService : INavigationService
@@ -14,7 +15,30 @@ public class NavigationService : INavigationService
     public IEnumerable<RouteModel> Routes { get; } = new List<RouteModel>
     {
         new("//users", typeof(LogInView), typeof(LogInViewModel)),
-        new("//users/home", typeof(UserOverviewView), typeof(UserOverviewViewModel))   
+        new("//users/add", typeof(AddUserView), typeof(AddUserViewModel)),
+
+        new("//home", typeof(UserOverviewView), typeof(UserOverviewViewModel)),
+        new("//home/all_projects", typeof(ProjectOverviewView), typeof(ProjectOverviewViewModel)),
+        new("//home/my_projects", typeof(UserProjectOverviewView), typeof(UserProjectOverviewViewModel)),
+        new("//home/activities", typeof(ActivityOverviewView), typeof(ActivityOverviewViewModel)),
+        new("//home/tags", typeof(TagOverviewView), typeof(TagOverviewViewModel)),
+        
+
+        new("//home/all_projects/add", typeof(AddProjectView), typeof(AddProjectViewModel)),
+        new("//home/all_projects/detail", typeof(AddProjectView), typeof(AddProjectViewModel)), //TODO detail
+        new("//home/all_projects/detail/edit", typeof(AddProjectView), typeof(AddProjectViewModel)), //TODO edit
+
+        new("//home/my_projects/add", typeof(AddProjectView), typeof(AddProjectViewModel)),
+        new("//home/my_projects/detail", typeof(AddProjectView), typeof(AddProjectViewModel)), //TODO detail
+        new("//home/my_projects/detail/edit", typeof(AddProjectView), typeof(AddProjectViewModel)), //TODO edit
+
+        new("//home/activities/add", typeof(AddUserView), typeof(AddUserViewModel)), //TODO add
+        new("//home/activities/detail", typeof(AddUserView), typeof(AddUserViewModel)), //TODO detail
+        new("//home/activities/detail/edit", typeof(AddUserView), typeof(AddUserViewModel)), //TODO edit
+
+        new("//home/tags/add", typeof(AddUserView), typeof(AddUserViewModel)), //TODO add
+        new("//home/tags/detail", typeof(AddUserView), typeof(AddUserViewModel)), //TODO detail
+        new("//home/tags/detail/edit", typeof(AddUserView), typeof(AddUserViewModel)), //TODO edit
     };
 
     public async Task GoToAsync<TViewModel>()
