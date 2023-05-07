50f19b0b15808a078ced5dadabe2f42618543bc6
Jurgee (Jiří Štípek) <jiristipek@email.cz>
2023-05-08 01:39:58 +0200

    add Id for edit

--- Actie/Actie.App/ViewModels/Activity/EditActivityViewModel.cs
+++ Actie/Actie.App/ViewModels/Activity/EditActivityViewModel.cs
@@ -1,18 +1,43 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using Actie.App.Messages;
 using Actie.App.Services;
+using Actie.BL.Facades.Interfaces;
+using Actie.BL.Models;
+using CommunityToolkit.Mvvm.Messaging;
+using Windows.System;
+
 
 namespace Actie.App.ViewModels;
-public partial class EditActivityViewModel : ViewModelBase
+[QueryProperty(nameof(Id), nameof(Id))]
+public partial class EditActivityViewModel : ViewModelBase, IRecipient<UserEditMessage>, IRecipient<UserDeleteMessage>
 {
-    public EditActivityViewModel(IMessengerService messengerService) : base(messengerService)
+    private readonly IUserFacade _userFacade;
+    private readonly INavigationService _navigationService;
+
+    public Guid Id { get; set; }
+    public UserDetailModel? User { get; private set; }
+    public EditActivityViewModel(IUserFacade userFacade, INavigationService navigationService, IMessengerService messengerService)
+        : base(messengerService)
     {
+        _userFacade = userFacade;
+        _navigationService = navigationService;
+    }
+    protected override async Task LoadDataAsync()
+    {
+        await base.LoadDataAsync();
+
+        User = await _userFacade.GetAsync(Id);
+    }
+    public async void Receive(UserEditMessage message)
+    {
+        if (message.UserId == User?.Id)
+        {
+            await LoadDataAsync();
+        }
+    }
+
+    public async void Receive(UserDeleteMessage message)
+    {
+        await LoadDataAsync();
     }
 }
