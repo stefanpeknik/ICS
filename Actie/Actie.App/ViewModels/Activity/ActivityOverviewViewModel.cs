eb957ec5f871ec772022538f1dbfd1a99670f810
Štefan Pekník <xpekni01@stud.fit.vutbr.cz>
2023-05-08 07:59:19 +0200

    add basic filtering to activity overview

--- Actie/Actie.App/ViewModels/Activity/ActivityOverviewViewModel.cs
+++ Actie/Actie.App/ViewModels/Activity/ActivityOverviewViewModel.cs
@@ -19,13 +19,86 @@ public partial class ActivityOverviewViewModel : ViewModelBase
     public Guid Id { get; set; }
 
     [ObservableProperty]
-    private IEnumerable<ActivityListModel> activities = Array.Empty<ActivityListModel>();
+    private IEnumerable<ActivityListModel> activities = new List<ActivityListModel>();
+
+    private DateTime _fromDate = DateTime.Today - TimeSpan.FromDays(30);
+    public DateTime FromDate
+    {
+        get => _fromDate;
+        set
+        {
+            if (_fromDate != value)
+            {
+                _fromDate = value;
+                OnPropertyChanged();
+                FilterOut();
+            }
+        }
+    }
+
+    private DateTime _toDate = DateTime.Today.AddDays(30);
+    public DateTime ToDate
+    {
+        get => _toDate;
+        set
+        {
+            if (_toDate != value)
+            {
+                _toDate = value;
+                OnPropertyChanged();
+                FilterOut();
+            }
+        }
+    }
+
+    private TimeSpan _fromTime = new(0, 0, 0);
+    public TimeSpan FromTime
+    {
+        get => _fromTime;
+        set
+        {
+            if (_fromTime != value)
+            {
+                _fromTime = value;
+                OnPropertyChanged();
+                FilterOut();
+            }
+        }
+    }
+
+    private TimeSpan _toTime = TimeSpan.FromDays(1) - TimeSpan.FromTicks(1);
+    public TimeSpan ToTime
+    {
+        get => _toTime;
+        set
+        {
+            if (_toTime != value)
+            {
+                _toTime = value;
+                OnPropertyChanged();
+                FilterOut();
+            }
+        }
+    }
+
+    
 
     public ActivityOverviewViewModel( IActivityFacade activityFacade, INavigationService navigationService, IMessengerService messengerService)
         : base(messengerService)
     {
         _activityFacade = activityFacade;
         _navigationService = navigationService;
+    }
+
+    
+    private void FilterOut()
+    {
+        var fromDateCombined = new DateTime(FromDate.Year, FromDate.Month, FromDate.Day, FromTime.Hours,
+            FromTime.Minutes, FromTime.Seconds);
+        var toDateCombined = new DateTime(ToDate.Year, ToDate.Month, ToDate.Day, ToTime.Hours, ToTime.Minutes, ToTime.Seconds);
+        Activities = _activityFacade.GetFilteredBeforeOrAfterDateTime(Id, fromDateCombined, toDateCombined)
+                         .GetAwaiter().GetResult()
+                     ?? Array.Empty<ActivityListModel>();
     }
 
     [RelayCommand]
