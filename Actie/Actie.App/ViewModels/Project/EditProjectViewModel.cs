15a81a06943d1b693bb358dd3003f7079ad724c1
gzvv <49114668+gzvvyx@users.noreply.github.com>
2023-05-08 05:56:12 +0200

    add Edit functionality, hopefully

--- Actie/Actie.App/ViewModels/Project/EditProjectViewModel.cs
+++ Actie/Actie.App/ViewModels/Project/EditProjectViewModel.cs
@@ -2,17 +2,64 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Actie.App.Messages;
 using Actie.App.Services;
+using Actie.BL.Facades.Interfaces;
+using Actie.BL.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace Actie.App.ViewModels;
-public class EditProjectViewModel : ViewModelBase
+[QueryProperty(nameof(Project), nameof(Project))]
+public partial class EditProjectViewModel : ViewModelBase
 {
-    public EditProjectViewModel(IMessengerService messengerService) : base(messengerService)
+    private readonly IProjectFacade _projectFacade;
+    private readonly INavigationService _navigationService;
+
+    [ObservableProperty]
+    public ProjectDetailModel project = ProjectDetailModel.Empty;
+    public EditProjectViewModel(
+        IProjectFacade projectFacade,
+        INavigationService navigationService,
+        IMessengerService messengerService)
+        : base(messengerService)
     {
+        _projectFacade = projectFacade;
+        _navigationService = navigationService;
     }
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await _projectFacade.SaveAsync(Project);
+
+        MessengerService.Send(new ProjectEditMessage { ProjectId = Project.Id });
+
+        _navigationService.SendBackButtonPressed();
+    }
+
+
+    [RelayCommand]
+    private async Task DeleteAsync()
+    {
+        if (Project is not null)
+        {
+            await _projectFacade.DeleteAsync(Project.Id);
+
+            MessengerService.Send(new ProjectDeleteMessage());
+
+            _navigationService.SendBackButtonPressed();
+
+        }
+    }
+
+
+    private async Task ReloadDataAsync()
+    {
+        Project = await _projectFacade.GetAsync(Project.Id)
+               ?? ProjectDetailModel.Empty;
+    }
+
+    
 }
--- Actie/Actie.App/ViewModels/Tag/EditTagViewModel.cs
+++ Actie/Actie.App/ViewModels/Tag/EditTagViewModel.cs
@@ -2,17 +2,64 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 //
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Actie.App.Messages;
 using Actie.App.Services;
+using Actie.BL.Facades.Interfaces;
+using Actie.BL.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
 
 namespace Actie.App.ViewModels;
+[QueryProperty(nameof(Tag), nameof(Tag))]
 public partial class EditTagViewModel : ViewModelBase
 {
-    public EditTagViewModel(IMessengerService messengerService) : base(messengerService)
+    private readonly ITagFacade _tagFacade;
+    private readonly INavigationService _navigationService;
+
+    [ObservableProperty]
+    public TagDetailModel tag = TagDetailModel.Empty;
+    public EditTagViewModel(
+        ITagFacade tagFacade,
+        INavigationService navigationService,
+        IMessengerService messengerService)
+        : base(messengerService)
     {
+        _tagFacade = tagFacade;
+        _navigationService = navigationService;
     }
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        await _tagFacade.SaveAsync(Tag);
+
+        MessengerService.Send(new TagEditMessage { TagId = Tag.Id });
+
+        _navigationService.SendBackButtonPressed();
+    }
+
+
+    [RelayCommand]
+    private async Task DeleteAsync()
+    {
+        if (Tag is not null)
+        {
+            await _tagFacade.DeleteAsync(Tag.Id);
+
+            MessengerService.Send(new TagDeleteMessage());
+
+            _navigationService.SendBackButtonPressed();
+
+        }
+    }
+
+
+    private async Task ReloadDataAsync()
+    {
+        Tag = await _tagFacade.GetAsync(Tag.Id)
+                  ?? TagDetailModel.Empty;
+    }
+
+
 }
